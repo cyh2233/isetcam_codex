7d9b70f6c6ecfdef28d109aa3d6f95983ae1147f
SeongcheolJeong <mrsc129@gmail.com>
2025-06-05 05:06:46 +0900

    Enable sphinx-gallery tutorials

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,13 +8,22 @@ sys.path.insert(0, os.path.abspath('../../python'))
 project = 'ISETCam'
 copyright = f'{datetime.now().year}, ISETCam'
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx_gallery.gen_gallery',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+sphinx_gallery_conf = {
+    'examples_dirs': os.path.abspath('../../python/tutorials'),
+    'gallery_dirs': 'tutorials',
+}
 
 # Automatically generate API docs
 def run_apidoc(app):
